cae9ff92878b6c3c8ed203a0ce3cdd006960a985
Cody Graham <codydgraham@gmail.com>
2020-01-25 11:07:57 -0500

    Added a menubar

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,11 +2,18 @@
 
 #include <QShortcut>
 #include <QMessageBox>
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->setWindowTitle("PathCAD");
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), this, SLOT(close()));
+    auto menuBar = new QMenuBar(this);
+    auto fileMenu = menuBar->addMenu("File");
+    auto exitAction = fileMenu->addAction("Exit");
+
+    QObject::connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
 }
 
 MainWindow::~MainWindow()
@@ -15,8 +22,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::close()
 {
-    QMessageBox::StandardButton reply;
-    reply = QMessageBox::question(this, "PathCAD", "Are you sure you want to quit?", QMessageBox::Yes|QMessageBox::No);
+    auto reply = QMessageBox::question(this, "PathCAD", "Are you sure you want to quit?", QMessageBox::Yes|QMessageBox::No);
     if (reply == QMessageBox::Yes)
         QMainWindow::close();
 }
